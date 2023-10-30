aab87391d0f4efffc3273a679e90d462d80e6c2c
Naviganet <dadegi@gmail.com>
2023-10-30 14:30:26 +0100

    Codice lezione settimana 2 giorno 1

---
+++ sett2/G1/lezione/assets/js/script.js
@@ -0,0 +1,12 @@
+document.write('Questa riga verrà scritta a fine pagina');
+
+document.getElementById('titolo').innerHTML = '<em>Hello, World!</em>';
+
+// window.alert('Ciao, sono un alert'); // Generalmente window si omette e si indica soltanto il metodo alert()
+// alert('Ciao, sono un alert');
+
+console.log('Ciao, sono la console');
+
+function scherzo() {
+    document.write('<h1>CIAOOOOOO!!!!!!!!</h1>');
+}
