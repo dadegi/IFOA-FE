2e74754fe38d842317b93eacc2ff1a2844443a70
Naviganet <dadegi@gmail.com>
2023-11-02 14:54:37 +0100

    Codici lezione settimana 2 giorno 3

--- sett2/G3/lezione/assets/js/app.js
+++ sett2/G3/lezione/assets/js/app.js
@@ -3,10 +3,11 @@
 /*
 1. LEGGERE IL CAMPO NOME E MEMORIZZARLO
 2. LEGGERE IL CAMPO ANNO E MEMORIZZARLO
-3. CALCOLARE L'ETÀ
-4. VERIFICA LA MAGGIORE O MINORE ETÀ
-5. SCRIVERE NELL'HTML
-6. CANCELLARE IL FORM
+3. CONTROLLARE LA VALIDITÀ DEL FORM
+4. CALCOLARE L'ETÀ
+5. VERIFICA LA MAGGIORE O MINORE ETÀ
+6. SCRIVERE NELL'HTML
+7. CANCELLARE IL FORM
 
 EVENTO SCATENANTE
 click
@@ -22,19 +23,31 @@ let anno; // globale in quanto utilizzata per leggere il campo di input, calcola
 let eta;
 let stato; // globale in quanto utilizzata per verificare l'età e stampare a video
 let errore = document.getElementById('errore');
+let esegui;
 
 function eventHandler() {
     leggiForm();
-    if (anno >=1900 && anno <= 2023 && nome != '') {
-        errore.innerHTML = '';
+    controlla();
+    if (esegui) {
         calcolaEta();
         verifica();
         scrivi();
         cancellaForm();
     } else {
-        errore.innerHTML= 'Attenzione! Compilare tutti i campi e inserire un numero tra 1900 e 2023';
+        errore.innerHTML = 'Attenzione! Compilare correttamente tutti i campi e scrivere un numero tra 1900 e 2023!';
         return;
     }
+}
+
+function controlla() {
+    esegui = false;
+    if (anno >= 1900 && anno <= 2023) {
+        if (nome != '') {
+            errore.innerHTML = '';
+            esegui = true;
+        }
+    }
+    return esegui;
 }
 
 function leggiForm() {
@@ -48,4 +61,14 @@ function calcolaEta() {
 
 function verifica() {
     stato = (eta >= 18) ? 'maggiorenne' : 'minorenne';
+}
+
+function scrivi() {
+    document.getElementById('mioNome').innerHTML = 'Ciao ' + nome;
+    document.getElementById('miaVerifica').innerHTML = 'La tua età è ' + eta + '; sei ' + stato;
+}
+
+function cancellaForm() {
+    document.getElementById('nome').value = '';
+    document.getElementById('anno').value = '';
 }
