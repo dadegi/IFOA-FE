17512472e7e82bb2d9a5ee76b9a89e5ee43c95c6
Naviganet <dadegi@gmail.com>
2023-11-07 12:44:52 +0100

    Codice lezione settimana 3 giorno 1

---
+++ sett3/G1/lezione/assets/js/script.js
@@ -0,0 +1,58 @@
+// METODO SPLICE
+
+const mioArray = ['Anna', 'Mario', 'Daniele', 'Giovanna', 'Nicola'];
+const nome1 = mioArray.splice(1, 2); // Con splice elimino i valori presenti nell'array mioArray e li inserisco in un nuovo array chiamato nom1
+
+const nuovoArray = [...mioArray, ...nome1] // Con lo spread operator inserisco tutti i valori di entrambi gli array in nuovoArray
+
+console.log(nuovoArray);
+
+mioArray.splice(0, 2, 'Gianni', 'Antonio'); // Il metodo splice MODIFICA l'array iniziale, che resta modificato in TUTTO lo script, non solo dopo lo splice
+
+console.log(mioArray);
+
+// --- Altro esempio
+
+const arrayPosizioni = ['primo', 'secondo', 'terzo'];
+console.log(arrayPosizioni);
+
+for (let i = 0; i < arrayPosizioni.length; i++) {
+  document.getElementById('testo').innerHTML += `<li>${arrayPosizioni[i]}</li>`
+}
+
+for (let i = 0; i < arrayPosizioni.length; i++) {
+  if (arrayPosizioni[i] === 'secondo') {
+    arrayPosizioni.splice(i, 1, 'nuovo secondo');
+  }
+  document.getElementById('testo2').innerHTML += `<li>${arrayPosizioni[i]}</li>`
+}
+
+console.log(arrayPosizioni);
+
+// METODO SLICE
+// Crea una fetta dell'array SENZA MODIFICARE l'array iniziale
+const disney = ['pippo', 'pluto', 'paperino', 'gastone', 'archimede', 'nonna papera'];
+
+const preferiti = disney.slice(1, 5);
+
+console.log(disney);
+console.log(preferiti);
+
+// METODO forEach
+// NON CREA un nuovo array, si limita ad eseguire la stessa funzione per ogni elemento dell'array
+const numeri1 = [2, 4, 6, 8, 10];
+
+numeri1.forEach(function(el) {
+    console.log(el * 3);
+});
+
+// METODO MAP
+// CREA un nuovo array, applicando a ogni elemento dell'array la funzione indicata. Generalmente il metodo map ha un return
+
+const arraySomma = numeri1.map(function(el) {
+    return el + 5;
+});
+
+console.log(arraySomma);
+
+// NESSUNO DEI DUE METODI modifica l'array di partenza
