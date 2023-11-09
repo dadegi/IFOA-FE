07972070545036fd70d52fbc6b212bf6d0830d8e
Naviganet <dadegi@gmail.com>
2023-11-09 10:08:36 +0100

    esercizio settimana 3 giorno 2

--- sett3/G2/esercizio/assets/js/script.js
+++ sett3/G2/esercizio/assets/js/script.js
@@ -102,9 +102,9 @@ paintItGreen();
 
 const makeItClickable = function () {
 	const titolo = document.querySelector('h1');
-	titolo.onclick = function() {
+	titolo.onclick = function () {
 		titolo.innerText = titolo.innerText.slice(0, -1);
-	}
+	};
 };
 
 makeItClickable();
@@ -115,10 +115,10 @@ makeItClickable();
 
 const revealFooterLink = function () {
 	const footer = document.querySelector('footer');
-	footer.onclick = function() {
+	footer.onclick = function () {
 		const link = document.querySelector('footer a');
 		alert(link.getAttribute('href'));
-	}
+	};
 };
 
 revealFooterLink();
@@ -128,51 +128,51 @@ revealFooterLink();
   La tabella avrà 5 elementi e questa struttura: immagine, nome prodotto, quantità, prezzo
 */
 
-const generateTable = function () {
-	const prodotti = [
-		{
-			id: 1,
-			immagine: 'immagine1',
-			nomeProd: 'prodotto 1',
-			quantita: 25,
-			prezzo: 57
-		},
-		{
-			id: 2,
-			immagine: 'immagine2',
-			nomeProd: 'prodotto 2',
-			quantita: 31,
-			prezzo: 18
-		},
-		{
-			id: 3,
-			immagine: 'immagine3',
-			nomeProd: 'prodotto 3',
-			quantita: 12,
-			prezzo: 93
-		},
-		{
-			id: 4,
-			immagine: 'immagine4',
-			nomeProd: 'prodotto 4',
-			quantita: 38,
-			prezzo: 25
-		},
-		{
-			id: 5,
-			immagine: 'immagine5',
-			nomeProd: 'prodotto 5',
-			quantita: 64,
-			prezzo: 37
-		},
-	]
+const prodotti = [
+	{
+		id: 1,
+		immagine: 'immagine1',
+		nomeProd: 'prodotto 1',
+		quantita: 25,
+		prezzo: 57,
+	},
+	{
+		id: 2,
+		immagine: 'immagine2',
+		nomeProd: 'prodotto 2',
+		quantita: 31,
+		prezzo: 18,
+	},
+	{
+		id: 3,
+		immagine: 'immagine3',
+		nomeProd: 'prodotto 3',
+		quantita: 12,
+		prezzo: 93,
+	},
+	{
+		id: 4,
+		immagine: 'immagine4',
+		nomeProd: 'prodotto 4',
+		quantita: 38,
+		prezzo: 25,
+	},
+	{
+		id: 5,
+		immagine: 'immagine5',
+		nomeProd: 'prodotto 5',
+		quantita: 64,
+		prezzo: 37,
+	},
+];
 
+const generateTable = function () {
 	const contenitore = document.getElementById('tableArea');
 	const tabella = document.createElement('table');
 	tabella.classList.add('tabella');
 
 	const primaRiga = document.createElement('tr');
-	
+
 	const intImmagine = document.createElement('th');
 	intImmagine.innerText = 'Immagine';
 	const intNome = document.createElement('th');
@@ -216,22 +216,93 @@ generateTable();
   Crea una funzione che aggiunga una riga alla tabella precedentemente creata e fornisca i dati necessari come parametri
 */
 
-const addRow = function () {};
+const addRow = function () {
+	const nuovoProdotto = {
+		id: 6,
+		immagine: 'immagine6',
+		nomeProd: 'prodotto 6',
+		quantita: 18,
+		prezzo: 25,
+	};
+
+	prodotti.push(nuovoProdotto);
+
+	const tabella = document.querySelector('table');
+
+	const nuovaRiga = document.createElement('tr');
+
+	let i = prodotti.length - 1;
+
+	const cellaImmagine = document.createElement('td');
+	cellaImmagine.innerText = `${prodotti[i].immagine}`;
+	const cellaNome = document.createElement('td');
+	cellaNome.innerText = `${prodotti[i].nomeProd}`;
+	const cellaQty = document.createElement('td');
+	cellaQty.innerText = `${prodotti[i].quantita}`;
+	const cellaPrezzo = document.createElement('td');
+	cellaPrezzo.innerText = `${prodotti[i].prezzo}`;
+
+	nuovaRiga.appendChild(cellaImmagine);
+	nuovaRiga.appendChild(cellaNome);
+	nuovaRiga.appendChild(cellaQty);
+	nuovaRiga.appendChild(cellaPrezzo);
+	tabella.appendChild(nuovaRiga);
+
+	nuovaRiga.appendChild(cellaImmagine);
+	nuovaRiga.appendChild(cellaNome);
+	nuovaRiga.appendChild(cellaQty);
+	nuovaRiga.appendChild(cellaPrezzo);
+	tabella.appendChild(nuovaRiga);
+};
+
+addRow();
 
 /* ESERCIZIO 14
  Crea una funzione che nasconda le immagini della tabella quando eseguita
 */
 
-const hideAllImages = function () {};
+// const hideAllImages = function () {
+// 	const immagini = document.querySelectorAll('td img');
+// 	immagini.forEach((element) => (element.style.display = 'none'));
+// };
 
 /* EXTRA ESERCIZIO 15
  Crea una funzione che cambi il colore del h2 con id "changeMyColor" con un colore random ad ogni click ricevuto
 */
 
-const changeColorWithRandom = function () {};
+const changeColorWithRandom = function () {
+	let cambiaColore = document.getElementById('changeMyColor');
+	cambiaColore.style.cursor = 'pointer';
+	cambiaColore.onclick = function() {
+		let red = Math.floor(Math.random() * 256);
+		let green = Math.floor(Math.random() * 256);
+		let blue = Math.floor(Math.random() * 256);
+
+		let coloreRandom = `rgb(${red},${green},${blue})`;
+
+		cambiaColore.style.color = coloreRandom;
+	}
+};
+
+changeColorWithRandom();
 
 /* EXTRA ESERCIZIO 16
  Crea una funzione che elimini le vocali da ogni elemento testuale della pagina (puoi aiutarti con i nuovi metodi degli array di ES6)
 */
 
-const deleteVowels = function () {};
+const deleteVowels = function () {
+	document.querySelectorAll('h1, h2, li, a, p, h3, th, td').forEach((element) => {  // selettore querySelectorAll multiplo
+		element.innerText = [...element.innerText].filter((carattere) => { // descrizione di una stringa di testo come array con lo spread operator
+			carattere = carattere.toLowerCase();
+			return ( // restituzione di elementi filtrati a condizione
+				carattere !== 'a' &&
+				carattere !== 'e' &&
+				carattere !== 'i' &&
+				carattere !== 'o' &&
+				carattere !== 'u'
+			);
+		}).join('') // fusione degli elementi nell'array fltrato in parola unica
+	});
+};
+
+// deleteVowels();
